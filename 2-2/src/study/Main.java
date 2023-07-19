cba4c598e20d925b5f1308d5dc2fc668bb538e70
momosetatsuya <129292182+momosetatsuya@users.noreply.github.com>
2023-07-19 22:37:07 +0900

    Add files via upload

---
+++ 2-2/src/study/Main.java
@@ -0,0 +1,15 @@
+package study;
+
+public class Main {
+	 
+    public static void main(String[] args) {
+ 
+        // ① 以下のルールに従いUserクラスのインスタンスを生成してください。
+        // 変数名:「user」
+    	User user = new User("Road",123,"road123");
+ 
+        // ② ①のインスタンス変数より、アカウント情報をコンソール出力するメソッドを呼び出しなさい。
+    	user.printAccountInfo();
+ 
+    }
+}
